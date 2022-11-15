5a48325f4dfe3347cb510ae389bea6f5d8d96595
Ubuntu <ubuntu@ip-172-31-24-46.ec2.internal>
2022-11-15 21:33:23 +0000

    feat: pushing fine tuning files

--- delete_checkpoints.py
+++ delete_checkpoints.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
-    dir = "/home/ubuntu/EMQA/train_cache_output_eng+tsquad_att1/checkpoint-{}/"
+    dir = "/home/ubuntu/EMQA/train_cache_output_eng+tsquad_att_test/checkpoint-{}/"
     save_steps = 500
-    for i in range(1500, 10000, 500):
+    for i in range(23000, 28500, 500):
         del_dir = dir.format(i)
         # os.remove(del_dir)
         print(f"rm -rf {del_dir}")
